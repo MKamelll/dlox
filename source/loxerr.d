4e24620d5effda1d614f8efc7f9225650dce881e
Mina <minallkamel@gmail.com>
2021-02-21 00:32:12 +0200

    add error handling to parser

--- source/loxerr.d
+++ source/loxerr.d
@@ -3,11 +3,21 @@ module loxerr;
 import std.stdio;
 import std.conv;
 import std.array;
+import loxer : Token, TokenType;
+
 
 class Loxerr
 {
     static void error(int line, string message) {
         report(line, "", message);
+    }
+
+    static void error(Token token, string message) {
+      if (token.type == TokenType.EOF) {
+        report(token.line, " at end", message);
+      } else {
+        report(token.line, " at '" ~ token.lexeme ~ "'", message);
+      }
     }
 
     private static void report(int line, string where,
